d360f0d5160ee5dbe8bcb67ed8f04485ed88e3e5
Mani02-11 <manikandan021103@gmail.com>
2026-09-25 22:57:42 +0530

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.project.exp7"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
